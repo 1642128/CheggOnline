fcdc8d9e8c6cf2ae13e0bafe686e75e0740a248e
1642128 <jmwill321jack2@gmail.com>
2026-03-13 09:51:06 -0400

    Create an 8x8 board with dynamic cells
    Initialize an 8x8 board by creating and appending cells.

--- main.js
+++ main.js
@@ -1,1 +1,9 @@
+const board = document.getElementById("board");
+
+const size = 8; // 8x8
+for (let i = 0; i < size * size; i++) {
+  const cell = document.createElement("div");
+  cell.className = "board_slot";  // or team logic here
+  board.appendChild(cell);
+}
 
